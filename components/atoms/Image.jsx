1d2e0830bfc835775f7a6866c77b4723d44d473c
John Jeo Molina <YOUR_GITHUB_EMAIL@cjeomolina40@gmail.com>
2026-09-01 17:08:17 +0800

    Updated website design

--- components/atoms/Image.jsx
+++ components/atoms/Image.jsx
@@ -1,10 +1,22 @@
-export default function HeritageImage({ src, alt, className = '' }) {
+
+import Image from 'next/image';
+
+export default function HeritageImage({
+  src,
+  alt,
+  className = '',
+}) {
   return (
-    <img
+    <Image
       className={className}
       src={src}
       alt={alt}
+      width={800}
+      height={500}
+      sizes="(max-width: 560px) 94vw, (max-width: 800px) 48vw, 350px"
       loading="lazy"
+      decoding="async"
     />
   );
 }
+
